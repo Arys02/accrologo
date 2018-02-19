4e8fae3c26958f35aa14ef025e1534d5242ef48a
charles yaiche <charles.yaiche@epita.fr>
2018-02-19 12:25:13 +0100

    Init Project
    Add primal winform interface

---
+++ Accrologo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accrologo
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Pen pen1 = new System.Drawing.Pen(Color.Blue, 2F);
+        private Image img = global::Accrologo.Properties.Resources.discord1;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private PictureBox newpic(uint id)
+        {
+            myPictureBox pf = new myPictureBox(id);
+            pf.SizeMode = PictureBoxSizeMode.StretchImage;
+            pf.Height = 53;
+            pf.Width = 53;
+            pf.Image = img;
+            return pf;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_clic(object sender, EventArgs e)
+        {
+            layoutPanel.Controls.Add(newpic(1));
+        }
+
+        private void test_run(object sender, EventArgs e)
+        {
+            g = draw_box.CreateGraphics();
+            g.DrawLine(pen1, 250, 50, 400, 200);
+
+            foreach (myPictureBox VARIABLE in layoutPanel.Controls)
+            {
+
+                //call parser 
+                // parser.parse(variable.id)
+                Console.WriteLine(VARIABLE.Image);
+                Console.WriteLine(VARIABLE.id);
+            }
+        }
+    }
+}
---
+++ Accrologo/my_PictureBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Accrologo
+{
+    public class myPictureBox : PictureBox
+    {
+        public uint id;
+
+        public myPictureBox(uint id)
+        {
+            this.id = id;
+        }
+    }
+}
