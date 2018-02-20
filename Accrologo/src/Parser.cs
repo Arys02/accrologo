69402bc1511f28667ece5bbb78b3b600179ccf10
BuildTools <unconfigured@null.spigotmc.org>
2018-02-20 21:32:23 +0100

    Parser.cs : implement parser, few static usefull fonction and forward

--- Accrologo/src/Parser.cs
+++ Accrologo/src/Parser.cs
@@ -23,22 +23,97 @@ namespace Accrologo
          * <sub_execution> := 
          * | id
          */
-            
-         /**
-          * TOKEN *
-          * FORWARD
-          * LEFT
-          * RIGHT
-          * REPEAT
-          * LEFTBRACE "["
-          * RIGHTBRACE "]"
-          * TO
-          * END
-          */
 
+        /**
+         * TOKEN *
+         * FORWARD
+         * LEFT
+         * RIGHT
+         * REPEAT
+         * LEFTBRACE "["
+         * RIGHTBRACE "]"
+         * TO
+         * END
+         */
+
+        private Dictionary<string, int> tokenIntDict = new Dictionary<string, int>()
+        {
+            {"ZERO" , 0 },
+            {"ONE"  , 1 },
+            {"TWO"  , 2 },
+            {"THREE", 3 },
+            {"FOUR" , 4 },
+            {"FIVE" , 5 },
+            {"SIX"  , 6 },
+            {"SEVEN", 7 },
+            {"EIGHT", 8 },
+            {"NINE" , 9 }
+        };
+
+        private List<string> tokenInstrList = new List<string>()
+        {
+            "FORWARD",
+            "LEFT",
+            "RIGHT",
+            "REPEAT",
+        };
+
+        private List<string> tokenBraceList = new List<string>()
+        {
+            "LEFTBRACE",
+            "RIGHTBRACE"
+        };
         public Parser()
         {
 
+        }
+
+        public void Parse(Queue<string> tokenList, Tortoise turtle, System.Drawing.Graphics g)
+        {
+            while(tokenList.Count != 0)
+            {
+                string token = tokenList.Dequeue();
+
+                switch (token)
+                {
+                    case "FORWARD":
+                        System.Console.WriteLine("forward");
+                        parseForward(tokenList, turtle, g);
+                        break;
+                    case "LEFT":
+                        System.Console.WriteLine("left");
+                        break;
+                    case "RIGHT":
+                        System.Console.WriteLine("right");
+                        break;
+                }
+
+                    
+            }
+        }
+
+        public void parseForward(Queue<string> tokenList, Tortoise turtle, System.Drawing.Graphics g)
+        {
+            if (tokenList.Count != 0 && tokenIntDict.ContainsKey(tokenList.Peek()))
+            {
+                int number = getNumber(tokenList);
+                turtle.updateAndMove(number, g);
+            }
+        }
+
+        public int getNumber(Queue<string> tokenList)
+        {
+            int number = 0;
+
+            int tokenNumber;
+            while(tokenList.Count != 0 && tokenIntDict.ContainsKey(tokenList.Peek()))
+            {
+                if (tokenIntDict.TryGetValue(tokenList.Dequeue(), out tokenNumber))
+                {
+                    number = number * 10 + tokenNumber;
+                }
+            }
+            return number;
         }
     }
 }
